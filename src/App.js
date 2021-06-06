3223e62eaa28e5eec3c5285331d669e08c3a3d0d
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-06 10:35:06 +0700

    rh-5-useState-TodoForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.scss';
+import TodoForm from './components/TodoForm';
 import TodoList from './components/ToDoList';
 
 function App() {
@@ -16,9 +17,23 @@ function App() {
     newTodoList.splice(index, 1);
     setTodoList(newTodoList);
   }
+  function handleTodoFormSubmit(formValues) {
+    console.log('Form Submit: ', formValues);
+    // add new todo to current todo list
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="app">
       <h1>React Hooks- TodoList</h1>
+      <TodoForm
+        onSubmit={handleTodoFormSubmit}
+      />
       <TodoList
         todos={todoList}
         onTodoClick={handleTodoClick}
