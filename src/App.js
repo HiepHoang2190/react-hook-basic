179bb6ba15f50f49fcff0885f40de9953022c023
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-06 09:46:46 +0700

    rh-4-useState-todoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components';
+import TodoList from './components/ToDoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend! 😍 ' },
+    { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+    { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+  ]);
+  function handleTodoClick(todo) {
+    // console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="app">
-      <h1>Welcome to React Hooks!</h1>
-      <ColorBox />
+      <h1>React Hooks- TodoList</h1>
+      <TodoList
+        todos={todoList}
+        onTodoClick={handleTodoClick}
+      />
     </div>
   );
 }
